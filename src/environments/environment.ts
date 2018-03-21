eff14154c820ee845a653c943c9481c81a9dd1f6
SAMBIT DAS <sambitdas55@gmail.com>
2018-03-21 18:38:39 +0530

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,15 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig :
+  {
+    apiKey: 'AIzaSyBnii7MLPmU1LZqA7c0mnBnDRG_CHmoI8I',
+    authDomain: 'angular5firebasecrud-cc4a8.firebaseapp.com',
+    databaseURL: 'https://angular5firebasecrud-cc4a8.firebaseio.com',
+    projectId: 'angular5firebasecrud-cc4a8',
+    storageBucket: '',
+    messagingSenderId: '974036826687'
+  }
+
 };
